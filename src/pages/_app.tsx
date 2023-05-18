ad4162996548f6d99e1f20521df2aa12e5cd1b6c
silva4dev <web4dev.lucas@outlook.com>
2023-05-18 19:44:00 -0300

    inicial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,16 @@ const App: React.FC<AppProps> = ({
         <Providers>
             <Head>
                 <title>Comic Vine</title>
+                <meta charSet="utf-8" />
+                <meta
+                    name="descripton"
+                    content="Comic Vine is the largest comic book wiki in the universe"
+                />
+                <meta name="author" content="Lucas Alves" />
+                <meta
+                    name="keywords"
+                    content="Comic Book, Wiki in the universe Comic Vine"
+                />
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1"
