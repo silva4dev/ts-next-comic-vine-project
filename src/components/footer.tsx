47519eb39541c19e54cfd7675c4f12a79e043102
silva4dev <web4dev.lucas@outlook.com>
2023-05-19 10:27:23 -0300

    inicial

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 
 export function Footer() {
     return (
-        <footer className="bg-light-500 p-5 flex items-center justify-end max-sm:justify-center">
+        <footer className="bg-light-500 p-5 flex items-center flex-wrap justify-end max-sm:justify-center">
             <p className="mr-10 text-[20px] flex items-center gap-[6px]">
                 Developed with
                 <FaHeart className="inline-block text-red-500" />
