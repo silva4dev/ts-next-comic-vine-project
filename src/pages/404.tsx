6c646c876aad421fcee34987d5a3ad928b0b2f13
silva4dev <web4dev.lucas@outlook.com>
2023-05-18 23:48:19 -0300

    inicial

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <section className="flex flex-col justify-center items-center">
-            <h1 className="text-blue-500 text-3xl">Not Found</h1>
-            <p className="text-gray-500">Could not find requested resource</p>
+        <section className="flex flex-col justify-center items-center border gap-4 h-screen">
+            <h1 className="text-green-500 text-7xl">Not Found</h1>
+            <p className="text-xl text-gray-500 mb-6">
+                Could not find requested resource
+            </p>
+            <Link
+                href="/"
+                className="bg-green-500 w-max p-3 rounded-md text-xl text-white hover:filter hover:brightness-95"
+            >
+                Back to home page
+            </Link>
         </section>
     );
 }
